d7c476cc7f8b94577b04b12ead86ca5adfcbcc16
salman-dev <salman-dev@outlook.com>
2022-05-20 00:38:09 +0500

    End of day 2022-05-22

---
+++ WebApp/Controllers/TicketsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class TicketsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+    }
+}
